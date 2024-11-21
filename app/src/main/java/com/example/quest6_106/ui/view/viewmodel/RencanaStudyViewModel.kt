c16edce2a8cb288bc51d47f62e43d8f8c2c6e78d
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:45:08 +0700

    Menambahkan fungsi untuk mengatur mata kuliah, kelas, dan menyimpan data KRS pada RencanaStudi

--- app/src/main/java/com/example/quest6_106/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -5,8 +5,26 @@ import com.example.quest6_106.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { statekelas -> statekelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status ->
+            status.copy(
+                namaMK = ls[0],
+                kelas = ls[1]
+            )
+        }
+    }
 }
