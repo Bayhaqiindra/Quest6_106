36a9bd2386fb77700fb1a26bd5ef103e085aab62
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:51:08 +0700

    Menambahkan NavHost untuk mengatur navigasi aplikasi dengan start destination Splash

--- app/src/main/java/com/example/quest6_106/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/quest6_106/navigation/PengelolaHalaman.kt
@@ -1,10 +1,12 @@
 package com.example.quest6_106.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.quest6_106.ui.view.viewmodel.MahasiswaViewModel
 import com.example.quest6_106.ui.view.viewmodel.RencanaStudyViewModel
@@ -25,4 +27,12 @@ fun NavigationControl(
 ) {
     val mahasiswaUiState = mahasiswaViewModel.statusUI.collectAsState().value
     var rencanaStudyState = rencanaStudyViewModel.krsStateUi.collectAsState().value
+
+    NavHost(
+        modifier = Modifier.padding(),
+        navController = navController,
+        startDestination = Halaman.Splash.name
+    ) {
+
+    }
 }
