ea110ff12177ad497d730346f218527761155246
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 01:26:51 +0700

    Menambahkan struktur Row dalam MahasiswaFormView untuk penempatan komponen dengan padding

--- app/src/main/java/com/example/quest6_106/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/MahasiswaFormView.kt
@@ -2,7 +2,10 @@ package com.example.quest6_106.ui.view.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.quest6_106.R
 
 @Composable
@@ -32,6 +36,11 @@ fun MahasiswaFormView(
                     id = R.color.primary)
             ), horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
 
+        }
     }
 }
