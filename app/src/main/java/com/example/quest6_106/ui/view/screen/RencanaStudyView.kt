c1f8f51ed67a16d290048f903136009da809a0c0
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 01:43:27 +0700

    Menyusun layout dasar untuk RencanaStudyview dengan Column dan Row

--- app/src/main/java/com/example/quest6_106/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/RencanaStudyView.kt
@@ -1,10 +1,22 @@
 package com.example.quest6_106.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.quest6_106.R
 
 @Composable
 fun RencanaStudyview(
@@ -22,4 +34,21 @@ fun RencanaStudyview(
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihankelas)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)
+            )
+    ) {
+        Spacer(modifier = Modifier.padding(top = 20.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+        }
+    }
 }
