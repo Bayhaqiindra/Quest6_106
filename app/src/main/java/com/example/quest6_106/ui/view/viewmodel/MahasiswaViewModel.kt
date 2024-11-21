ac05c348e7c03289a2edbd2a12722861221d9ef5
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:39:58 +0700

    Menambahkan fungsi saveDataSiswa untuk menyimpan data mahasiswa ke dalam ViewModel

--- app/src/main/java/com/example/quest6_106/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -5,8 +5,19 @@ import com.example.quest6_106.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel: ViewModel() {
     private val _statusUI = MutableStateFlow(Mahasiswa())
     val statusUI: StateFlow<Mahasiswa> = _statusUI.asStateFlow()
+
+    fun saveDataSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2],
+            )
+        }
+    }
 }
