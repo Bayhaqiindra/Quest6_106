53ced35bdaff825abd33651ef5e61cbb9ed34c8f
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 01:29:53 +0700

    Menguploud image ke drawble dan Menambahkan logo dan teks header pada MahasiswaFormView dengan elemen Row dan Column

--- app/src/main/java/com/example/quest6_106/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/MahasiswaFormView.kt
@@ -1,11 +1,14 @@
 package com.example.quest6_106.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,8 +16,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quest6_106.R
 
 @Composable
@@ -40,7 +47,27 @@ fun MahasiswaFormView(
         Row(
             verticalAlignment = Alignment.CenterVertically,
         ) {
-
+            Image(
+                painter = painterResource(
+                    id = R.drawable.umy
+                ),
+                contentDescription = "",
+                modifier = Modifier.size(45.dp)
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul Dan Islami" ,
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+            }
         }
     }
 }
