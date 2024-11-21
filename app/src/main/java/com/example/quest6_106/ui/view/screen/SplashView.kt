8586472f41ad72545da55b9c360c342addffa736
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:18:24 +0700

    Membuat Function SplashView dan Menambahkan layout dasar untuk SplashView dengan background warna utama

--- app/src/main/java/com/example/quest6_106/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/SplashView.kt
@@ -1,2 +1,32 @@
 package com.example.quest6_106.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quest6_106.R
+
+@Preview(showBackground = true)
+@Composable
+fun SplashView(
+    onMulaiButton: () -> Unit = {},
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
