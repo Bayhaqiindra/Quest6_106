9c01f289b67e1ae3a011a5fe680b1caf967b2578
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:26:40 +0700

    Menambahkan layout Column dengan background berwarna dan padding atas di TampilDataView

--- app/src/main/java/com/example/quest6_106/ui/view/screen/TampilData.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/TampilData.kt
@@ -1,6 +1,15 @@
 package com.example.quest6_106.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.quest6_106.R
 import com.example.quest6_106.model.Mahasiswa
 import com.example.quest6_106.model.RencanaStudi
 
@@ -11,5 +20,16 @@ fun TampilDataView(
     onBackButton: () -> Unit,
     onSplashButton: () -> Unit
 ){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary)
+            )
+            .padding(top = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
 }
