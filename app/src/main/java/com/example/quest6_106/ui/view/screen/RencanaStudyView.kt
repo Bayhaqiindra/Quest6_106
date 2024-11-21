fa6e537b815de938b2e0fa58142e0bdb9223136b
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 01:41:28 +0700

    Menambahkan RencanaStudyview composable dengan dropdown dan checkbox untuk form rencana studi

--- app/src/main/java/com/example/quest6_106/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,25 @@
 package com.example.quest6_106.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun RencanaStudyview(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {
+    var chosenDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihankelas by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihankelas)
+}
