efcca91305c38f4191c5c3dea0308af97c413296
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:19:12 +0700

    Menambahkan gambar logo UMY pada SplashView

--- app/src/main/java/com/example/quest6_106/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/SplashView.kt
@@ -1,14 +1,18 @@
 package com.example.quest6_106.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quest6_106.R
 
 @Preview(showBackground = true)
@@ -27,6 +31,12 @@ fun SplashView(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Image(
+            painter = painterResource(
+                id = R.drawable.umy
+            ),
+            contentDescription = "Logo UMY",
+            modifier = Modifier.size(150.dp)
+        )
     }
 }
