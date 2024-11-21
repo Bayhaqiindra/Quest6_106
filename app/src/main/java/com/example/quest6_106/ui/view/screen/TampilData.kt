8e6cd8634acc9d3fee0e08e1ea8db4200f8abbe3
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:25:28 +0700

    Menambahkan fungsi TampilDataView untuk menampilkan data mahasiswa dan rencana studi, dengan tombol navigasi kembali dan ke layar splash

--- app/src/main/java/com/example/quest6_106/ui/view/screen/TampilData.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/screen/TampilData.kt
@@ -1,2 +1,15 @@
 package com.example.quest6_106.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import com.example.quest6_106.model.Mahasiswa
+import com.example.quest6_106.model.RencanaStudi
+
+@Composable
+fun TampilDataView(
+    mahasiswa : Mahasiswa,
+    rencanastudy : RencanaStudi,
+    onBackButton: () -> Unit,
+    onSplashButton: () -> Unit
+){
+
+}
