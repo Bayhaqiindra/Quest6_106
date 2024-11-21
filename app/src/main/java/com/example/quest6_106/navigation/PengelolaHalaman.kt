93dbd5d08505801dc19919b3b57dc96d107906c6
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:50:31 +0700

    Menambahkan NavigationControl composable untuk mengelola navigasi dan state mahasiswa serta rencana studi dan menambahkan build gradle

--- app/src/main/java/com/example/quest6_106/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/quest6_106/navigation/PengelolaHalaman.kt
@@ -1,8 +1,28 @@
 package com.example.quest6_106.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.quest6_106.ui.view.viewmodel.MahasiswaViewModel
+import com.example.quest6_106.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     MataKuliah,
     TampilData
+}
+
+@Composable
+fun NavigationControl(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    rencanaStudyViewModel: RencanaStudyViewModel = viewModel(),
+    navController : NavHostController = rememberNavController()
+) {
+    val mahasiswaUiState = mahasiswaViewModel.statusUI.collectAsState().value
+    var rencanaStudyState = rencanaStudyViewModel.krsStateUi.collectAsState().value
 }
