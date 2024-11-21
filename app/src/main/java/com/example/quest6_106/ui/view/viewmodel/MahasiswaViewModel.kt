bb61eceb72545c9ef248407419e54ae463bf6ab1
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:39:08 +0700

    Menambahkan ViewModel untuk mengelola data Mahasiswa dengan StateFlow

--- app/src/main/java/com/example/quest6_106/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_106/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.quest6_106.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.quest6_106.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MahasiswaViewModel: ViewModel() {
+    private val _statusUI = MutableStateFlow(Mahasiswa())
+    val statusUI: StateFlow<Mahasiswa> = _statusUI.asStateFlow()
 }
