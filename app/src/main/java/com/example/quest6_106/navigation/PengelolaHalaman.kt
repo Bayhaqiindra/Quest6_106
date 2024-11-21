c037cd35dd381fa2ba26ee9c39dd40e071c67ff1
Bayhaqiindra <bayhaqi30072004.com>
2024-11-22 02:52:20 +0700

    Menambahkan navigasi antar halaman menggunakan NavController untuk Splash, Mahasiswa, MataKuliah, dan TampilData

--- app/src/main/java/com/example/quest6_106/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/quest6_106/navigation/PengelolaHalaman.kt
@@ -7,7 +7,12 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.quest6_106.ui.view.screen.MahasiswaFormView
+import com.example.quest6_106.ui.view.screen.RencanaStudyview
+import com.example.quest6_106.ui.view.screen.SplashView
+import com.example.quest6_106.ui.view.screen.TampilDataView
 import com.example.quest6_106.ui.view.viewmodel.MahasiswaViewModel
 import com.example.quest6_106.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -33,6 +38,47 @@ fun NavigationControl(
         navController = navController,
         startDestination = Halaman.Splash.name
     ) {
-
+        composable(
+            route = Halaman.Splash.name
+        ) {
+            SplashView(onMulaiButton = {
+                navController.navigate(
+                    Halaman.Mahasiswa.name
+                )
+            })
+        }
+        composable(route = Halaman.Mahasiswa.name) {
+            MahasiswaFormView(
+                onSubmitButtonClicked = {mahasiswaViewModel.saveDataSiswa(it)
+                    navController.navigate(
+                        Halaman.MataKuliah.name
+                    )},
+                onBackButtonClicked = {navController.popBackStack()}
+            )
+        }
+        composable(route = Halaman.MataKuliah.name) {
+            RencanaStudyview(
+                mahasiswa = mahasiswaUiState,
+                onSubmitButtonClicked = {rencanaStudyViewModel.saveDataKRS(it)
+                    navController.navigate(
+                        Halaman.TampilData.name){
+                    }
+                },
+                onBackButtonClicked = { navController.popBackStack() }
+            )
+        }
+        composable(route = Halaman.TampilData.name) {
+            TampilDataView(
+                mahasiswa = mahasiswaUiState,
+                rencanastudy = rencanaStudyState,
+                onBackButton = { navController.popBackStack() },
+                onSplashButton = {
+                    navController.popBackStack(
+                        route = Halaman.Splash.name,
+                        inclusive = false
+                    )
+                }
+            )
+        }
     }
 }
